91902a20f0e900f59a27872e6047ceda76613f11
zixuan28 <zlin28@uw.edu>
2024-05-21 20:37:56 -0700

    Updated with descriptive stat as well as new plots

--- Code.py
+++ Code.py
@@ -4,47 +4,15 @@ import numpy as np
 import statsmodels.api as sm
 import statsmodels.formula.api as smf
 
-#Data
+#Data Cleaning & Presenting
 df = pd.read_csv('file path')
 df.set_index('Date', inplace=True) # set the date as the index
+df
 
-#Model with all variables
-formula = 'House_Price_Index ~ ' + ' + '.join(
-    df.columns.difference(['House_Price_Index'])) #get all the columns except for the house price index
+#Descriptive Statistics
+df.describe() #descriptive statistics for each column
 
-model = smf.ols(formula, data=df)
-results = model.fit()
-
-print(results.summary())
-
-#Check for autocorrelation, heteroskedasticity, and multicollinearity of our model
-from statsmodels.stats.stattools import durbin_watson
-from statsmodels.stats.diagnostic import het_breuschpagan
-from statsmodels.stats.outliers_influence import variance_inflation_factor
-
-X = df.drop('House_Price_Index', axis=1)
-y = df['House_Price_Index']
-
-X = sm.add_constant(X)
-model = sm.OLS(y, X).fit()
-
-residuals = model.resid
-
-dw_result = durbin_watson(residuals) # Apply Durbin-Watson test on the residuals
-print(f'Durbin-Watson statistic: {dw_result}')
-
-bp_test = het_breuschpagan(residuals, model.model.exog) # Apply Breusch-Pagan test
-labels = ['Lagrange multiplier statistic', 'p-value', 'f-value', 'f p-value']
-print(dict(zip(labels, bp_test)))
-
-vif = pd.DataFrame()
-vif["variables"] = X.columns
-vif["VIF"] = [variance_inflation_factor(X.values, i) for i in range(X.shape[1])] # Calculate VIF for each explanatory variable
-print(vif)
-
-#It appears there are all three issues present in our model. We will address them one by one.
-
-#plots
+#Data Visualization for each variable depends on time
 variables = df.columns
 plt.figure(figsize=(15, 30))  # Increase the figure size to 15 inches wide by 30 inches tall
 for i, var in enumerate(variables, start=1):
@@ -55,5 +23,24 @@ for i, var in enumerate(variables, start=1):
     plt.ylabel(var)
     plt.xticks(rotation=45)
     plt.tight_layout()
+    plt.xticks([df.index.min(), df.index.max()]) #Remove the crowded x-axis labels but kept the first and last labels
 
 plt.show()
+
+#Data Visualization for house price index depends on different variables
+for column in df.columns:
+    if column != 'House_Price_Index':
+        plt.scatter(df[column], df['House_Price_Index'])
+        plt.xlabel(column)
+        plt.ylabel('House Price Index')
+        plt.title(f'Relationship between {column} and House Price Index')
+        plt.show()
+
+#Model with all variables
+formula = 'House_Price_Index ~ ' + ' + '.join(
+    df.columns.difference(['House_Price_Index'])) #get all the columns except for the house price index
+
+model = smf.ols(formula, data=df)
+results = model.fit()
+
+print(results.summary())
